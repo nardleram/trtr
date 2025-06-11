cccc2822bbe708fbd7b0190a9dba09a71aae8314
Toby Russell <tobyrussell@protonmail.com>
2025-06-11 12:29:55 +0100

    CSS issue with labels in login form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,14 +10,20 @@
         <form action="{{ route('login') }}" method="POST" class="font-sans font-light text-textBase">
             @csrf
 
-            <label for="name" class="absolute pl-4 pt-2 text-xs text-slate-400">Name</label>
-            <input name="name" class="w-full mt-1 pl-4 pt-9 pb-4 shadow-sm h-8 rounded-lg focus:border-primaryBlue focus:ring-1 focus:ring-primaryBlue focus:shadow-md text-sm border-b border-slate-400 mb-2" type="text" required autofocus>
+            <div class="w-full">
+                <label for="name" class="absolute pl-4 pt-2 text-xs text-slate-400">Name</label>
+                <input name="name" class="w-full mt-1 pl-4 pt-9 pb-4 shadow-sm h-8 rounded-lg focus:border-primaryBlue focus:ring-1 focus:ring-primaryBlue focus:shadow-md text-sm border-b border-slate-400 mb-2" type="text" required autofocus>
+            </div>
 
-            <label for="email" class="absolute pl-4 pt-2 text-xs text-slate-400">Email</label>
-            <input name="email" class="w-full mt-1 pl-4 pt-9 pb-4 shadow-sm h-8 rounded-lg focus:border-primaryBlue focus:ring-1 focus:ring-primaryBlue focus:shadow-md text-sm border-b border-slate-400 mb-2" type="email" required>
+            <div class="w-full">
+                <label for="email" class="absolute pl-4 pt-2 text-xs text-slate-400">Email</label>
+                <input name="email" class="w-full mt-1 pl-4 pt-9 pb-4 shadow-sm h-8 rounded-lg focus:border-primaryBlue focus:ring-1 focus:ring-primaryBlue focus:shadow-md text-sm border-b border-slate-400 mb-2" type="email" required>
+            </div>
 
-            <label for="password" class="absolute pl-4 pt-2 text-xs text-slate-400">Password</label>
-            <input name="password" class="w-full mt-1 pl-4 pt-9 pb-4 shadow-sm h-8 rounded-lg focus:border-primaryBlue focus:ring-1 focus:ring-primaryBlue focus:shadow-md text-sm border-b border-slate-400 mb-2" type="password" required>
+            <div class="w-full">
+                <label for="password" class="absolute pl-4 pt-2 text-xs text-slate-400">Password</label>
+                <input name="password" class="w-full mt-1 pl-4 pt-9 pb-4 shadow-sm h-8 rounded-lg focus:border-primaryBlue focus:ring-1 focus:ring-primaryBlue focus:shadow-md text-sm border-b border-slate-400 mb-2" type="password" required>
+            </div>
 
             <button type="submit" class="w-full mt-3 py-2 bg-gradient-to-b from-rose-800 via-rose-600 to-rose-800 rounded-lg text-sm font-semibold text-rose-50 hover:shadow-md hover:text-white transition-all delay-100 duration-300">Log in</button>
         </form>
